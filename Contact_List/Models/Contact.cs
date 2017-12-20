59d1b49a0c4b052204d917a47026a0d13af54d88
Renato Katalenić <Renato.Katalenic@outlook.com>
2017-12-20 13:08:40 +0100

    Small corrections.

--- Contact_List/Models/Contact.cs
+++ Contact_List/Models/Contact.cs
@@ -10,19 +10,25 @@ namespace Contact_List.Models
     public class Contact
     {
         public int Id { get; set; }
+
         [Required]
         [DisplayName("Name")]
         public string Name { get; set; }
+
         [Required]
         [DisplayName("Surname")]
         public string Surname { get; set; }
+
         [Required]
         [DisplayName("Phone")]
         public string Phone { get; set; }
+
         [EmailAddress]
         [DisplayName("Email address")]
         public string Email { get; set; }
+
         public DateTime CreatedOn { get; set; }
+
         public DateTime ModifiedOn { get; set; }
     }
 }
