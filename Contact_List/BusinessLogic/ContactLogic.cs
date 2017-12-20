3df98c0af865012d923480fc345dd211456f563c
Renato Katalenić <Renato.Katalenic@outlook.com>
2017-12-20 12:22:40 +0100

    Added contatc stuff.

---
+++ Contact_List/BusinessLogic/ContactLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Contact_List.BusinessLogic
+{
+    public class ContactLogic
+    {
+
+    }
+}
